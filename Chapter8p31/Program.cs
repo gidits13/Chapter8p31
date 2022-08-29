f2a4b0bfba7d72f17f6fa40c3a9dcd9989232754
gidits13 <gidits13@gmail.com>
2022-08-29 12:52:14 +0300

    Добавьте файлы проекта.

---
+++ Chapter8p31/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace Chapter8p31
+{
+    internal class Program
+    {
+        static void ReadFileToConsole()
+        {
+            string path = @"C:\repos\Chapter8p31\Chapter8p31\Program.cs";
+            if(File.Exists(path))
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string str = "";
+                    while ((str = sr.ReadLine()) != null)
+                            
+                    {
+                        Console.WriteLine(str);
+                    }
+
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ReadFileToConsole();
+        }
+    }
+}
